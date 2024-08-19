879d04e9c54871c739860d10c0c39c4003920ab3
zhuzhuxia <742491842@qq.com>
2024-08-19 22:04:18 +0800

    leetcode: 169. Majority Element

--- cpp/169. Majority Element.h
+++ cpp/169. Majority Element.h
@@ -5,4 +5,26 @@
 #ifndef CPP_169_MAJORITY_ELEMENT_H
 #define CPP_169_MAJORITY_ELEMENT_H
 
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int> &nums) {
+        map<int, int> m;
+        for (auto &e: nums) {
+            m[e]++;
+        }
+        int n = nums.size();
+        for (auto &e: m) {
+            if (e.second > n / 2) {
+                return e.first;
+            }
+        }
+        return 0;
+    }
+
+};
+
 #endif //CPP_169_MAJORITY_ELEMENT_H
