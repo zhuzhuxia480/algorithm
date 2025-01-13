f26c94fe29a871062ee670df3e1babf257b0e8e6
zhuzhuxia480 <742491842@qq.com>
2025-01-13 11:00:13 +0800

    206.reverse-linked-list.go

--- go/206.reverse-linked-list.go
+++ go/206.reverse-linked-list.go
@@ -13,24 +13,42 @@
  * }
  */
 package main
+// func reverseList(head *ListNode) *ListNode {
+// 	if head == nil || head.Next == nil {
+// 		return head
+// 	}
+// 	pPre := (*ListNode)(nil)
+// 	pCur := head
+// 	pNext := pCur.Next
+	
+// 	for pCur != nil {
+// 		pCur.Next = pPre
+// 		pPre = pCur
+// 		pCur = pNext
+// 		if pCur == nil {
+// 			return pPre
+// 		}
+// 		pNext = pCur.Next
+// 	}	
+// 	return head
+// }
+
+var newHead *ListNode
+func dfs(node *ListNode) {
+	if node.Next == nil {
+		newHead = node
+		return
+	}
+	dfs(node.Next)
+	node.Next.Next = node
+}
 func reverseList(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
-	pPre := (*ListNode)(nil)
-	pCur := head
-	pNext := pCur.Next
-	
-	for pCur != nil {
-		pCur.Next = pPre
-		pPre = pCur
-		pCur = pNext
-		if pCur == nil {
-			return pPre
-		}
-		pNext = pCur.Next
-	}	
-	return head
+	dfs(head)
+	head.Next = nil
+	return newHead
 }
 // @lc code=end
 
